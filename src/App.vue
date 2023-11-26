f6160f589391f6604d5e36a1a9525ebd8264df18
gaoting <837082729@qq.com>
2023-11-26 11:32:23 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useDebouncedRef } from './utils/test'
+const text = useDebouncedRef('hello', 1000)
+</script>
+
+<template>
+  <p>
+    This text only updates 1 second after you've stopped typing:
+  </p>
+  <p>{{ text }}</p>
+  <input v-model="text" />
+</template>
---
+++ src/views/child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ data }}</div>
+</template>
+ 
+<script>
+export default {
+  async setup() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await response.json()
+    
+    return {
+      data: data.title
+    }
+  }
+}
+</script>
