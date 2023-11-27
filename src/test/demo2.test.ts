9fe55902ceaf89ce15d710290b027a7dfa82efde
gaoting <837082729@qq.com>
2023-11-27 09:07:58 +0800

    feat: test demo

--- src/test/demo2.test.ts
+++ src/test/demo2.test.ts
@@ -3,20 +3,20 @@ import { describe, expect, test } from "vitest";
 
 import Demo2 from "../components/Demo2.vue";
 
-describe("world",()=>{
-  test("renders a message and responds correctly to user input",async()=>{
-    const wrapper = shallowMount(Demo2,{
+describe("world", () => {
+  test("renders a message and responds correctly to user input", async () => {
+    const wrapper = shallowMount(Demo2, {
       props: {
-        message: 'hello',
-        username: ''
-      }
-    })
+        message: "hello",
+        username: "",
+      },
+    });
 
-    expect(wrapper.find(".message").text()).toEqual("hello")
+    expect(wrapper.find(".message").text()).toEqual("hello1");
 
-    expect(wrapper.find(".error").exists()).toBeTruthy()
+    expect(wrapper.find(".error").exists()).toBeTruthy();
 
-    await wrapper.setProps({username: "Luna"})
-    expect(wrapper.find(".error").exists()).toBeFalsy()
-  })
-})
+    await wrapper.setProps({ username: "Luna" });
+    expect(wrapper.find(".error").exists()).toBeFalsy();
+  });
+});
